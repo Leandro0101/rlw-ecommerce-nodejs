3148b36e11899015503bf239bdec50c96b037fc6
Leandro0101 <leandrovieira3306@gmail.com>
2020-09-27 16:00:56 -0300

    adding CategoryController

--- server/src/routes.js
+++ server/src/routes.js
@@ -2,9 +2,12 @@ const express = require("express");
 const route = express.Router();
 const AddressController = require("./app/controllers/AddressController");
 const UserController = require("./app/controllers/UserController");
+const AuthController = require("./app/controllers/AuthController");
 
 route.post("/users", UserController.store);
 
 route.post("/users/:user_id/addresses", AddressController.store);
+
+route.post("/authentication", AuthController.authentication);
 
 module.exports = route;
