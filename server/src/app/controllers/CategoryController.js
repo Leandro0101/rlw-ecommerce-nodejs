85b7e5f7bd941bdb38f4151e5a949751596535d5
rodrigo12663 <ricardodedetizadora@hotmail.com>
2020-09-27 17:08:38 -0300

    crud de categories

--- server/src/app/controllers/CategoryController.js
+++ server/src/app/controllers/CategoryController.js
@@ -1,7 +1,5 @@
-
 const Category = require("../models/Category");
 const slugify = require("slugify");
-
 
 module.exports ={
     async create(req,res){
@@ -9,18 +7,9 @@ module.exports ={
     
         const category = await Category.create({name,slug:slugify(name),description});
         return res.status(201).json(category);
-<<<<<<< HEAD
-
-=======
->>>>>>> ad68b6ac4b6b9f04f4b6e7ff7b6bbadf97adbdff
     },
     async index(request,response){
         const category= await Category.findAll();
         return response.json({category});
-<<<<<<< HEAD
-=======
- 
-
->>>>>>> ad68b6ac4b6b9f04f4b6e7ff7b6bbadf97adbdff
     }
 }
