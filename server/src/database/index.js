2f315b53db4dff9ed88319ef4dbde486dd0bc1de
JamilsonMello <jamilsonmelow@gmail.com>
2020-09-27 17:15:14 -0300

    created feature discount coupon

--- server/src/database/index.js
+++ server/src/database/index.js
@@ -2,10 +2,13 @@ const Sequelize = require("sequelize");
 const dbConfig = require("../config/database");
 const User = require("../app/models/User");
 const Address = require("../app/models/Address");
+const Promotion = require("../app/models/Promotion");
+
 const connection = new Sequelize(dbConfig);
 
 User.init(connection);
 Address.init(connection);
+Promotion.init(connection);
 
 Address.associate(connection.models);
 User.associate(connection.models);
--- server/src/routes.js
+++ server/src/routes.js
@@ -2,9 +2,12 @@ const express = require("express");
 const route = express.Router();
 const AddressController = require("./app/controllers/AddressController");
 const UserController = require("./app/controllers/UserController");
+const PromotionController = require("./app/controllers/PromotionController");
 
 route.post("/users", UserController.store);
 
 route.post("/users/:user_id/addresses", AddressController.store);
+
+route.post("/promotions", PromotionController.store);
 
 module.exports = route;
