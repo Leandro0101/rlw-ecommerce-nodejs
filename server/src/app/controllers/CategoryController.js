ad68b6ac4b6b9f04f4b6e7ff7b6bbadf97adbdff
Rodrigo Vieira Nunes <62311509+rodrigo12663@users.noreply.github.com>
2020-09-27 16:54:11 -0300

    Update CategoryController.js

--- server/src/app/controllers/CategoryController.js
+++ server/src/app/controllers/CategoryController.js
@@ -9,13 +9,11 @@ module.exports ={
     
         const category = await Category.create({name,slug:slugify(name),description});
         return res.status(201).json(category);
-<<<<<<< HEAD
     },
     async index(request,response){
         const category= await Category.findAll();
         return response.json({category});
  
-=======
->>>>>>> ca4b92bc3a901b9f4f77a65041ed5bfb7b1ca27f
+
     }
 }
