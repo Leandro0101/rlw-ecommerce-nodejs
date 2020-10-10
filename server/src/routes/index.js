e8ab9fb2fc94d9a7e613ed700d3c523978979779
rodrigo12663 <ricardodedetizadora@hotmail.com>
2020-10-10 19:32:48 -0300

    crud product pt-2

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -14,6 +14,11 @@ app.use(express.json());
 //Não precisa de autenticação
 app.use(routerAuth);
 
+app.use(routerCategory);
+app.use(routerProduct);
+app.use( routerAddress);
+app.use( routerPromotion);
+
 //Essas rotas possuem métodos q precisam de autenticação, e também métodos que não precisam
 app.use(routerUser);
 app.use(routerProduct);
@@ -26,3 +31,4 @@ app.use(routerPromotion);
 app.use(routerAddress);
 
 export default app;
+
